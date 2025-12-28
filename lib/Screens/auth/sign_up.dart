4ae04846018cfa511aa64751ab41836d3a6b8bf7
Helia <heliashafiee707@gmail.com>
2025-12-28 13:05:21 +0330

    Add Sign up and Sign in authentication screens to new branch

--- lib/Screens/auth/sign_up.dart
+++ lib/Screens/auth/sign_up.dart
@@ -2,4 +2,21 @@ import 'package:flutter/material.dart';
 class SignUpScreen extends StatelessWidget{
   const SignUpScreen({super.key});
 
+  Widget build(BuildContext context){
+    return Scaffold(
+        backgroundColor: const Color(0xFF66AEEF),
+        body: SafeArea(
+          child: Padding(padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(onPressed:(){Navigator.pop(context);}
+                  , icon: const Icon(Icons.arrow_back),color: Color(0xFF061F5C))
+            )
+            
+          ],)),
+    )
+  }
+
 }
